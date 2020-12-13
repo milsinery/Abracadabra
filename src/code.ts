246d8386d2995fe469f622bd8dbd7d086bdc432f
Vladimir Bykov <milsinery@Milsinerys-MacBook-Air.local>
2020-12-13 17:38:16 +0300

    test change image functional

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,34 @@
 import faker from "faker";
 
-const selected = figma.currentPage.findAll((item) => findParent(item).type !== "COMPONENT" && item.name[0] === "$");
+const selected = figma.currentPage.findAll(
+  (item) => findParent(item).type !== "COMPONENT" && item.name[0] === "$"
+);
 const text = selected.filter((item) => item.type === "TEXT");
+const other = selected.filter((item) => item.type !== "TEXT");
 
-text.map((item) => replaceText(item.name, item));
-figma.closePlugin();
+figma.showUI(
+  `<script>
+    fetch(${JSON.stringify("https://source.unsplash.com/256x256/?portrait,smile")}).then(r => r.arrayBuffer()).then(a => parent.postMessage({ pluginMessage: { data: new Uint8Array(a) }}, '*'));
+  </script>`,
+  {
+    visible: false,
+  }
+);
+
+figma.ui.onmessage = (msg) => {
+  if (msg.data && msg.data.length > 0) {
+    other.map((item) => addImage(item.name, item, msg.data));
+  }
+  text.map((item) => replaceText(item.name, item));
+  figma.closePlugin();
+};
+
+function addImage(type, layer, data) {
+  if(type.toLocaleUpperCase() === "$AVATAR") {
+    let imageHash = figma.createImage(data).hash;
+    layer.fills = [{ type: "IMAGE", scaleMode: "FILL", imageHash }];
+  }
+}
 
 function findParent(obj) {
   return obj.parent.type === "PAGE" ? obj : findParent(obj.parent);
